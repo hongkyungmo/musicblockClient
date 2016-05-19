48ddc13029183e1ef667213d26fea7adb2c28232
keheunhye <asopn1994@daum.net>
2016-05-19 19:43:14 +0900

    에러상황 확인코드 추가

--- musicblockClient/www/javascript/common/loginModal.js
+++ musicblockClient/www/javascript/common/loginModal.js
@@ -79,7 +79,7 @@ function serverLogin(transUser,transPass,transRemember) {
 
 	$
 	.ajax({ //$.post(), $.get(), $.getJSON 등도 있음
-		url : 'http://localhost:8080/'+'user/userLogin',
+		url : 'http://192.168.0.111:8080/'+'user/userLogin',
 		type : 'POST', //Request하는 방식.
 		data : JSON.stringify({ //JSON.stringify를 해줘야 제대로 된 형태의 JSON이 날아감
 			user : transUser,
@@ -89,8 +89,13 @@ function serverLogin(transUser,transPass,transRemember) {
 		dataType : "json", //Response로 오는 방식. Request 타입을 지정하는 것으로 착각하기 쉬우므로 주의.
 		contentType : 'application/json;charset=UTF-8', //POST방식일 때 사용. 인코딩 안해주면 한글 깨져서 전송됨
 		success : function(data, status) {
-			console.log(status);
+			console.log('sucess'+status);
+			try{
 			console.log("JSONData1 : " + JSON.stringify(data));
+			}catch(e){
+				console.log(e);
+			}
+			
 			if (data['user'] !=null) {
 				console.log(data['user'].nick+'님 환영합니다!');
 				$('#loginModal').modal('hide');
@@ -106,8 +111,21 @@ function serverLogin(transUser,transPass,transRemember) {
 				alert('회원 정보가 없습니다. 다시 확인해 주세요.');
 			}
 		},
-		error : function(status) {
-			console.log('error'+status);
+		error : function(x,e) {
+			
+			if(x.status==0){
+				alert("인터넷이 꺼져있음");
+			}else if(x.status==404){
+				alert("404 에러");
+			}else if(x.status==500){
+				alert("서버 에러");
+			}else if(e=='parsererror'){
+				alert("에러 파싱 불가");
+			}else if(e=='timeout'){
+				alert("시간초과 에러");
+			}else{
+				alert("알 수 없는 에러");
+			}
 		}
 	});
 };
