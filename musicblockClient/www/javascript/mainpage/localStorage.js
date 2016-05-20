418bdf0cc1bb0d9da59374eb61f78d39e427e777
hongkyungmo <withious@naver.com>
2016-05-20 13:28:42 +0900

    메인화면 아이템 배경 투명으로 변경

--- musicblockClient/www/javascript/mainpage/localStorage.js
+++ musicblockClient/www/javascript/mainpage/localStorage.js
@@ -38,7 +38,7 @@ function getAllBlocks(){
 function displayOneMusic(value){
 	console.log(value);
 	var item = "<div class='swiper-slide'>"
-		+"<div class='ui blue button music'>"
+		+"<div class='ui blue button music' style='background-color:transparent'>"
 			+"<div class='noteVisualContainer'>"
 				+"<i class='fa fa-music fa-3x' id='playing' style='color:#cc4488;'></i>"
 			+"</div>"
